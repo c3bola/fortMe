48447acf958f87bcb274d3cf38b7cb637c2a9b47
c3bola <aneziofidalgo@gmail.com>
2026-03-26 07:02:12 -0300

    refactor: migrate scheduled jobs to MySQL
    Update scheduled jobs to use MySQL data layer.

--- cron/functions/analyzeFortMe.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (bot) => {
-  console.log('[INFO] Executando análise do FortMe...');
-  // Implementação futura da análise do FortMe
-  console.log('[INFO] Análise do FortMe concluída.');
-};
--- cron/functions/analyzeJonesy.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (bot) => {
-  console.log('[INFO] Executando análise do Jonesy...');
-  // Implementação futura da análise do Jonesy
-  console.log('[INFO] Análise do Jonesy concluída.');
-};
